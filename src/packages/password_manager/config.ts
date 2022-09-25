cbd4507bd498ea65429c49b733302ed5c4a41bdd
Guilherme Bohnstedt <guibohnstedt@gmail.com>
2022-09-24 23:05:48 -0300

    ✨new package password manager

---
+++ src/packages/password_manager/config.ts
@@ -0,0 +1,7 @@
+import type { PackageConfiguration } from "src/models";
+
+export default {
+    name: 'Password Manager',
+    description: 'Utilitario para gestão de senhas',
+    version: '0.0.0'
+} as PackageConfiguration
