ae99b7e1930751cd758f68e09cf20e76ab65287a
Guilherme Bohnstedt <guibohnstedt@gmail.com>
2022-09-27 02:00:01 -0300

    ✨render svelte component dynamic

--- src/lib/Dashboard.svelte
+++ src/lib/Dashboard.svelte
@@ -2,35 +2,58 @@
   import Grid from "svelte-grid";
   import type { GridCol, GridItem } from "svelte-grid";
   import gridHelp from "svelte-grid/build/helper";
-  import { onMount } from "svelte";
   import type { PackageConfiguration } from "src/models";
-
-  let items: GridItem[] = [];
+  import SpinnerLoader from "./SpinnerLoader.svelte";
 
   const cols: GridCol[] = [[1280, 12]];
+  let pkgs: PackageConfiguration[];
 
-  onMount(() => {
-    window.api.get<PackageConfiguration[]>("packages").then((response) => {
-      items = response.data.map((pkg, index) => ({
-        12: gridHelp.item({
-          x: index * 2,
-          y: 0,
-          w: 2,
-          h: 2,
-        }),
-        id: pkg.identifier,
-      }));
-    });
+  const packagesPromise = new Promise<GridItem[]>(async (resolve, reject) => {
+    window.api
+      .get<PackageConfiguration[]>("packages")
+      .then((response) => {
+        const items: GridItem[] = response.data.map((pkg, index) => ({
+          12: gridHelp.item({
+            x: index * 4,
+            y: 0,
+            w: 4,
+            h: 3,
+          }),
+          id: pkg.identifier,
+        }));
+        pkgs = response.data;
+        resolve(items);
+      })
+      .catch(reject);
   });
+
+  const getComponent = async (id: string) => {
+    const findedPackage = pkgs.find((pkg) => pkg.identifier === id);
+    return (
+      await import(`../../packages/${findedPackage.identifier}/preview.svelte`)
+    ).default;
+  };
 </script>
 
-<div class="container">
-  <Grid bind:items rowHeight={100} let:item {cols}>
-    <div class="widget content">
-      {item.id}
-    </div>
-  </Grid>
-</div>
+{#await packagesPromise}
+  <SpinnerLoader />
+{:then items}
+  <div class="container">
+    <Grid {items} rowHeight={100} let:dataItem {cols}>
+      <div class="widget content">
+        {#await getComponent(dataItem.id)}
+          <SpinnerLoader />
+        {:then component}
+          <svelte:component this={component} />
+        {:catch error}
+          <p style="color: red">{error}</p>
+        {/await}
+      </div>
+    </Grid>
+  </div>
+{:catch error}
+  <p style="color: red">{error}</p>
+{/await}
 
 <style>
   .widget {
