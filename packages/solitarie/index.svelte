4539ead653437bd04e0cbfb090823afa03dde84c
Guilherme Bohnstedt <guibohnstedt@gmail.com>
2022-09-29 01:05:07 -0300

    ✨solitarie package working

--- packages/solitarie/index.svelte
+++ packages/solitarie/index.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
+  export let isPreview: boolean;
 
+  const handleClick = () => window.open("https://solitaired.com");
 </script>
 
-Jogue Solitarie
+{#if isPreview}
+  <div
+    class="w-full h-full bg-[url('packages/solitarie/assets/banner_solitaire.jpg')] bg-center bg-cover"
+  >
+    <button
+      class="btn btn-wide glass absolute bottom-5 right-5"
+      on:click={handleClick}>Jogar</button
+    >
+  </div>
+{:else}
+  Nada
+{/if}
