d90f85c318fc31bcb157557872735032c44e2eb2
Guilherme Bohnstedt <guibohnstedt@gmail.com>
2022-09-30 01:32:32 -0300

    📦️add iconify remix

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,17 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import dynamicImport from "vite-plugin-dynamic-import";
 import { join } from "path";
+import Icons from "unplugin-icons/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svelte(),
     dynamicImport(),
+    Icons({
+      compiler: "svelte",
+      autoInstall: true,
+    }),
   ],
   resolve: {
     alias: [
