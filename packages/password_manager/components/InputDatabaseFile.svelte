b03bf25833146da7564089a895252ec2eb8b634d
Guilherme Bohnstedt <guibohnstedt@gmail.com>
2022-10-01 14:20:03 -0300

    💄adjust input styles

--- packages/password_manager/components/InputDatabaseFile.svelte
+++ packages/password_manager/components/InputDatabaseFile.svelte
@@ -7,12 +7,12 @@
     <input
       type="file"
       placeholder="Selecionar arquivo"
-      class="input input-bordered w-full block pr-10 pl-0 rounded-lg file:mr-4 file:py-2.5 file:px-4 file:rounded-btn file:border-0 file:font-semibold file:bg-gray-700 file:text-gray-300"
+      class="input input-bordered w-full block pr-10 pl-0 rounded-lg file:mr-4 file:py-2.5 file:px-4 file:rounded-btn file:border-0 file:font-semibold file:bg-zinc-600 file:text-gray-300"
       required
     />
 
     <div
-      class="flex absolute inset-y-0 right-0 items-center pr-3 pointer-events-none "
+      class="flex absolute inset-y-0 right-0 items-center pr-3 pointer-events-none"
     >
       <IconDatabase />
     </div>
