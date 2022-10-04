9e4ba0c9c04cdc411b4347f90fdcc46e1433745d
Guilherme Bohnstedt <guibohnstedt@gmail.com>
2022-10-04 19:24:47 -0300

    💄fix stopwatch numbers flashing

--- packages/stopwatch/index.svelte
+++ packages/stopwatch/index.svelte
@@ -8,25 +8,29 @@
   let elapsed = 0;
   let lastStartTime = getNow();
   let frame: number;
+  let isStarted = false;
 
   const getElapsed = () => getNow() - lastStartTime;
+
+  const updateTime = () => {
+    elapsed = getElapsed();
+    frame = requestAnimationFrame(updateTime);
+  };
 
   const handleReset = () => {
     lastStartTime = getNow();
     elapsed = getElapsed();
   };
 
-  const handleStop = () => {
+  const handlePause = () => {
     cancelAnimationFrame(frame);
+    isStarted = false;
   };
 
   const handleStart = () => {
     lastStartTime = getNow() - elapsed;
-
-    frame = requestAnimationFrame(function update() {
-      elapsed = getElapsed();
-      frame = requestAnimationFrame(update);
-    });
+    frame = requestAnimationFrame(updateTime);
+    isStarted = true;
   };
 
   $: clockString = new Date(elapsed)
@@ -36,18 +40,23 @@
 </script>
 
 {#if isPreview}
-  <div class="w-full h-full p-10">
-    <Card class="justify-center text-center w-full h-full max-w-full" size="lg" padding="lg">
-      <p class="text-9xl tracking-wide dark:text-white">{clockString}</p>
-
+  <div class="w-full h-full p-10 flex justify-center align-middle">
+    <Card class="justify-center text-center" size="lg" padding="lg">
+      <span class="flex text-8xl dark:text-white font-bold tabular-nums">
+        {clockString}
+      </span>
       <div
         class="justify-center items-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4"
       >
         <Button size="xl" color="alternative" on:click={handleReset}
           >Reiniciar</Button
         >
-        <Button size="xl" color="red" on:click={handleStop}>Parar</Button>
-        <Button size="xl" color="green" on:click={handleStart}>Iniciar</Button>
+        {#if isStarted}
+          <Button size="xl" color="red" on:click={handlePause}>Pausar</Button>
+        {:else}
+          <Button size="xl" color="green" on:click={handleStart}>Iniciar</Button
+          >
+        {/if}
       </div>
     </Card>
   </div>
