54e3bcf3a190b91830f4d06686c4034a6c6350f8
Guilherme Bohnstedt <guibohnstedt@gmail.com>
2022-10-05 13:09:33 -0300

    🩹add missing props for P

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -15,8 +15,8 @@
     alignment="mx-auto"
     alt="sample 1"
   />
-  <P size="xs" align="center">Versão: 0.0.0</P>
-  <P size="base" height="relaxed" 
+  <P size="xs" align="center" opacity={1} space="normal">Versão: 0.0.0</P>
+  <P size="base" height="relaxed" opacity={1} space="normal"
     >O Stax Utilities é uma aplicação que reune diversos utilitários para o dia
     a dia. No Dashboard é possível acompanhar dados importantes.</P
   >
