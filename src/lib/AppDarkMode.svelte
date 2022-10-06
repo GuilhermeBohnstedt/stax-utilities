2171e843febe93f6f780f61693693c0740186797
Guilherme Bohnstedt <guibohnstedt@gmail.com>
2022-10-06 02:17:16 -0300

    ✨custom dark mode component

---
+++ src/lib/AppDarkMode.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  let isDark = localStorage.getItem("color-theme") === "dark";
+
+  const toggleTheme = () => {
+    localStorage.setItem("color-theme", isDark ? "dark" : "light");
+    isDark = window.document.documentElement.classList.toggle("dark");
+  };
+</script>
+
+<svelte:head>
+  <script>
+    if (window) {
+      localStorage.getItem("color-theme") === "dark" ||
+      (!("color-theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+        ? window.document.documentElement.classList.add("dark")
+        : window.document.documentElement.classList.remove("dark");
+    }
+  </script>
+</svelte:head>
+
+<slot {isDark} {toggleTheme} />
