79fd4e0a581a724c4f91e19c51609c38d6a3daa9
Guilherme Bohnstedt <guibohnstedt@gmail.com>
2022-10-24 00:37:50 -0300

    🩹use default vite env vars

--- app/utils/get-plugins-path.ts
+++ app/utils/get-plugins-path.ts
@@ -1,9 +1,7 @@
 import { join } from "path";
 
 const getPluginsPath = () => {
-  console.log("node_ENV: ", process.env.NODE_ENV)
-  const path =
-    process.env.NODE_ENV === "development" ? "../../plugins" : "../plugins";
+  const path = import.meta.env.DEV ? "../../plugins" : "../plugins";
 
   return join(__dirname, path);
 };
